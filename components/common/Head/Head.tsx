a642996ed7414568f5d7b716cacfda342d30c378
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-07-20 16:16:37 +0700

    update

---
+++ components/common/Head/Head.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <meta
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
+      <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+    </>
+  );
+}
