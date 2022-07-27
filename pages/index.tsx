c578a3553780bde198d072a6e9c93842222197a9
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-07-27 17:46:53 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,38 @@ import Head from "next/head";
 import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Marquee from "react-fast-marquee";
+import { Box } from "@mui/system";
 
 export default function Home() {
   return (
     <Container>
       <Typography variant="body3">adadas</Typography>
+      <Box
+        sx={{
+          position: "absolute",
+          width: "100%",
+          left: 0,
+          backgroundColor: "lightGray",
+        }}
+      >
+        <Marquee pauseOnHover={true} speed={10}>
+          <Box>aaaa</Box>
+          <Box>bbbb</Box>
+          <Box>cccc</Box>
+          <Box>dddd</Box>
+          <Box>eeee</Box>
+          <Box>eeee</Box>
+        </Marquee>
+        <Marquee pauseOnHover={true} speed={10}>
+          <Box>aaaa</Box>
+          <Box>bbbb</Box>
+          <Box>cccc</Box>
+          <Box>dddd</Box>
+          <Box>eeee</Box>
+          <Box>eeee</Box>
+        </Marquee>
+      </Box>
     </Container>
   );
 }
