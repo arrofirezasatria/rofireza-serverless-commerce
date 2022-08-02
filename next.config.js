4bac8bf9529992cde10ba985987e00e8fadd4844
unknown <arrofirezasatria@gmail.com>
2022-08-03 06:56:55 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const STUDIO_REWRITE = {
+  source: "/sanity-cms/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/sanity-cms/:path*"
+      : "/sanity-cms/index.html",
+};
+
+module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
+};
